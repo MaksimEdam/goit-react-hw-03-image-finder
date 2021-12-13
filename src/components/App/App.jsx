2c48fad157ba0c47925d5e9bed1f170b1e1a279f
MaksimEdam <Edamenko6@gmail.com>
2021-12-13 21:29:45 +0200

    Searcbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,51 @@
 import axios from "axios";
 import Searchbar from "../Searchbar/Searchbar";
 import s from "./App.module.css";
-
 import React, { Component } from "react";
-
 import ImageGallery from "../ImageGallery/ImageGallery";
 
 class App extends Component {
   state = {
+    imageName: "",
     loading: false,
     items: [],
   };
+  handleFormSubmit = (imageName) => {
+    this.setState({ imageName });
+  };
+  /* 
   componentDidMount() {
     this.setState({ loading: true });
     axios
       .get(
-        "https://pixabay.com/api/?q=водка&page=1&key=18992166-123806360f211761da038f5eb&image_type=photo&orientation=horizontal&per_page=12"
+        `https://pixabay.com/api/?q=${this.state.imageName}&page=1&key=18992166-123806360f211761da038f5eb&image_type=photo&orientation=horizontal&per_page=12`
       )
       .then((result) => {
         this.setState({ items: result.data.hits });
       })
       .finally(() => this.setState({ loading: false }));
+  } */
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevState.imageName;
+    const nextName = this.state.imageName;
+    if (prevName !== nextName) {
+      this.setState({ loading: true });
+      axios
+        .get(
+          `https://pixabay.com/api/?q=${nextName}&page=1&key=18992166-123806360f211761da038f5eb&image_type=photo&orientation=horizontal&per_page=12`
+        )
+        .then((result) => {
+          this.setState({ items: result.data.hits });
+        })
+        .finally(() => this.setState({ loading: false }));
+    }
   }
-
   render() {
     const { items } = this.state;
     return (
       <div className={s.App}>
         {this.state.loading && <h2>Loading...</h2>}
-        <Searchbar />
+        <Searchbar onSubmit={this.handleFormSubmit} />
         <ImageGallery items={items} />
       </div>
     );
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,25 +1,46 @@
 import { ImSearch } from "react-icons/im";
 import s from "./Searchbar.module.css";
+import React, { Component } from "react";
+class Searchbar extends Component {
+  state = {
+    imageName: "",
+  };
 
-function Searchbar() {
-  return (
-    <header className={s.Searchbar}>
-      <form className={s.SearchForm}>
-        <button type="submit" className={s.SearchFormButton}>
-          <ImSearch style={{ marginRight: 8 }} />
-          <span className={s.SearchFormButtonLabel}>Search</span>
-        </button>
+  handleNameChange = (event) => {
+    this.setState({ imageName: event.currentTarget.value.toLowerCase() });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.imageName.trim() === "") {
+      alert("Введите имя");
+      return;
+    }
+    this.props.onSubmit(this.state.imageName);
+    this.setState({ imageName: "" });
+  };
 
-        <input
-          className={s.SearchFormInput}
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
+  render() {
+    return (
+      <header className={s.Searchbar}>
+        <form className={s.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={s.SearchFormButton}>
+            <ImSearch style={{ marginRight: 8 }} />
+            <span className={s.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={s.SearchFormInput}
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+            value={this.state.imageName}
+            onChange={this.handleNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
 }
 
 export default Searchbar;
