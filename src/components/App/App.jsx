159472b89490503d6c8ff63549adf515aa25533e
MaksimEdam <Edamenko6@gmail.com>
2021-12-14 10:41:31 +0200

    page searchbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,16 +3,17 @@ import Searchbar from "../Searchbar/Searchbar";
 import s from "./App.module.css";
 import React, { Component } from "react";
 import ImageGallery from "../ImageGallery/ImageGallery";
+import Button from "../Button/Button";
 
 class App extends Component {
+  myRef = React.createRef();
   state = {
     imageName: "",
     loading: false,
     items: [],
+    page: 1,
   };
-  handleFormSubmit = (imageName) => {
-    this.setState({ imageName });
-  };
+
   /* 
   componentDidMount() {
     this.setState({ loading: true });
@@ -28,25 +29,38 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     const prevName = prevState.imageName;
     const nextName = this.state.imageName;
-    if (prevName !== nextName) {
+    if (prevName !== nextName || prevState.page !== this.state.page) {
       this.setState({ loading: true });
       axios
         .get(
-          `https://pixabay.com/api/?q=${nextName}&page=1&key=18992166-123806360f211761da038f5eb&image_type=photo&orientation=horizontal&per_page=12`
+          `https://pixabay.com/api/?q=${nextName}&page=${this.state.page}&key=18992166-123806360f211761da038f5eb&image_type=photo&orientation=horizontal&per_page=12`
         )
+        /* .then((result) => {
+          this.setState({ items: result.data.hits }); */
         .then((result) => {
-          this.setState({ items: result.data.hits });
+          this.setState((prevState) => ({
+            items: [...prevState.items, ...result.data.hits],
+          }));
         })
         .finally(() => this.setState({ loading: false }));
     }
   }
+  handleFormSubmit = (imageName) => {
+    this.setState({ imageName });
+    this.setState({ items: [] });
+    this.myRef.current.scrollIntoView();
+  };
+  onBtnClick = () => {
+    this.setState((prevState) => ({ page: prevState.page + 1 }));
+  };
   render() {
     const { items } = this.state;
     return (
-      <div className={s.App}>
+      <div className={s.App} ref={this.myRef}>
         {this.state.loading && <h2>Loading...</h2>}
         <Searchbar onSubmit={this.handleFormSubmit} />
         <ImageGallery items={items} />
+        {this.state.items.length > 0 && <Button onBtnClick={this.onBtnClick} />}
       </div>
     );
   }
