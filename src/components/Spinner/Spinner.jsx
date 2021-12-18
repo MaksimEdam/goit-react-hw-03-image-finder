17ad7a320426457d23de401d685ee6cffd91036a
MaksimEdam <Edamenko6@gmail.com>
2021-12-18 16:24:55 +0200

    style spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,16 +1,18 @@
 import Loader from "react-loader-spinner";
 import React, { Component } from "react";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import s from "./Spinner.module.css";
 
 export default class Spinner extends Component {
   render() {
     return (
       <Loader
-        type="Puff"
+        className={s.loader}
+        type="BallTriangle"
         color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={3000}
+        height={150}
+        width={150}
+        timeout={1000}
       />
     );
   }
